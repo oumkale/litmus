ea9410f136c87de98f083e0c5213174295f06358
Oum Kale <oumkale@chaosnative.com>
2021-03-28 22:30:29 +0530

    s
    Signed-off-by: Oum Kale <oumkale@chaosnative.com>

--- litmus-portal/frontend/src/components/WorkflowCard/styles.ts
+++ litmus-portal/frontend/src/components/WorkflowCard/styles.ts
@@ -196,7 +196,7 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 3,
   },
   infraChaosMain: {
-    fontSize: '0.6255rem',
+    fontSize: '0.625rem',
     fontWeight: 500,
   },
 }));
